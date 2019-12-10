cdbd506bd17e2e811f7765e3fd2e5b1fe24bdcca
Sea <bnly22335@gmail.com>
2019-12-09 16:22:26 -0800

    PowerofThree(#326)

---
+++ PowerofThree.java
@@ -0,0 +1,15 @@
+public class PowerofThree {
+    public boolean isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        while (n % 3 == 0) {
+            n = n/3;
+        }
+        return n == 1;
+
+    }
+}
