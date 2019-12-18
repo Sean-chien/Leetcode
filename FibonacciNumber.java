b4aa1196acc5518057faf1e0bc41821d169f26d8
Sea <bnly22335@gmail.com>
2019-12-17 20:29:52 -0800

    Fibonacci Number(#509)

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -1,11 +1,23 @@
 public class FibonacciNumber {
     public int fib(int N) {
-        if (N == 0) {
-            return 0;
+//        if (N == 0) {
+//            return 0;
+//        }
+//        if (N == 1) {
+//            return 1;
+//        }
+//        return fib(N-1) + fib(N-2);
+        if (N <= 1) {
+            return N;
         }
-        if (N == 1) {
-            return 1;
+        int result = 0;
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 2; i < N; i++) {
+            result = num1 + num2;
+            num1 = num2;
+            num2 = result;
         }
-        return fib(N-1) + fib(N-2);
+        return result;
     }
 }
