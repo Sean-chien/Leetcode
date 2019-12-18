e559d09f353f0f13483d6ff1de8171dbc4d53400
Sea <bnly22335@gmail.com>
2019-12-17 19:44:06 -0800

    UglyNumber(#263)

---
+++ UglyNumber.java
@@ -0,0 +1,10 @@
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        for (int i = 2; i < 6; i++) {
+            while (num % i == 0) {
+                num /= i;
+            }
+        }
+        return num == 1;
+    }
+}
