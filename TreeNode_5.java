4c2f5bee72be9f2c16b98f72ca0c6c509829f89a
Sea <bnly22335@gmail.com>
2019-12-18 12:17:17 -0800

    TreeNode_5(#404)

---
+++ TreeNode_5.java
@@ -0,0 +1,22 @@
+public class TreeNode_5 {
+    int val;
+    TreeNode_5 left;
+    TreeNode_5 right;
+    TreeNode_5(int x) { val = x; }
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
+
